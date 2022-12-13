cb89b4573bd7b70cdc59d1490c11e9fd711800e1
luv-jeri <luvvjeri@gmail.com>
2022-12-13 16:25:07 +0530

    Added - Development message

--- components/Sections/About/index.jsx
+++ components/Sections/About/index.jsx
@@ -3,5 +3,9 @@ import About from './About';
 import Lines from '../../Wrappers/Lines/Lines.wrapper';
 
 export default function Index() {
-  return <About />;
+  return (
+    <Lines>
+      <About />;
+    </Lines>
+  );
 }
--- components/Sections/Hero/Hero.jsx
+++ components/Sections/Hero/Hero.jsx
@@ -7,14 +7,14 @@ export default function Hero() {
       <div className={s.fadeUp}></div>
       <div className={s.sayHello}>
         <h3>Hey, I am Sanjay</h3>
-        <h1>Let's make something good together </h1>
-        <h2>Fullstack,App Dev, Desginer</h2>
+        <h1>App is in development</h1>
+        <h2>right now it is not at all mobile usable</h2>
       </div>
       <div className={s.me}>
-        <img
+        {/* <img
           src='https://images.unsplash.com/photo-1523206489230-c012c64b2b48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
           width={200}
-        />
+        /> */}
       </div>
     </div>
   );
